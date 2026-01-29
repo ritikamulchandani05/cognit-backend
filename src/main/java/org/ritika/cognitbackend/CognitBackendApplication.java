f9984b556f981651d66ca9872f11c9990bdfa826
Ritika Mulchandani <ritikamulchandani05@gmail.com>
2026-01-29 21:24:22 +0530

    Chore: Initialized Project

---
+++ src/main/java/org/ritika/cognitbackend/CognitBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ritika.cognitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ritika/cognitbackend/CognitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ritika.cognitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
