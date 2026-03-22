04b657741d1d62cdad0cad60666743a27aa52338
Ritika Mulchandani <ritikamulchandani05@gmail.com>
2026-03-22 19:58:36 +0530

    fix: changed the ResourceNotFoundException class to handle Objects instead of String

--- src/main/java/org/ritika/cognitbackend/exception/ResourceNotFoundException.java
+++ src/main/java/org/ritika/cognitbackend/exception/ResourceNotFoundException.java
@@ -1,10 +1,8 @@
 package org.ritika.cognitbackend.exception;
 
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
-        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){ super(message);}
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue){
+        super(String.format("%s not found with %s: '%s'", resourceName,fieldName,fieldValue));
     }
 }
